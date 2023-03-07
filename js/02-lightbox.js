fb9d0ad31fbb9b26f7a4b978cb704b0175ee486e
AndriiKostenko84 <a.kostenko84@gmail.com>
2023-03-07 15:26:35 +0200

    hw2

--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -1,4 +1,25 @@
 import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
-console.log(galleryItems);
+
+const galleryEl = document.querySelector('.gallery')
+
+const createGallery = galleryItems
+    .map(({ preview, original, description }) => 
+    `<a class="gallery__item" href="${original}">
+    <img
+      class="gallery__image"
+      src="${preview}"
+      data-source="${original}"
+      alt="${description}"/>
+  </a>`
+    )
+    .join(''); 
+
+galleryEl.insertAdjacentHTML('beforeend', createGallery);
+
+new SimpleLightbox('.gallery a', {
+    captionSelector: 'img',
+    captionsData: 'alt',
+    captionsDelay: 250
+});
